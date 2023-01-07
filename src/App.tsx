6c5a6144c725b826e65d13270d4748a11f66315e
unknown <anterebol@gmail.com>
2023-01-07 23:02:52 +0300

    init: init project with game 2048

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>2048 game</p>
+    </div>
+  );
+}
+
+export default App;
